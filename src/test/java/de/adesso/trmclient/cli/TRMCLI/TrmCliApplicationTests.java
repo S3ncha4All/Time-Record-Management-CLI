d6eae9a6499c0d0d8358d0e41d3708695b6562cf
Ross <jonathan.ross@adesso.de>
2023-07-21 15:21:20 +0200

    Project init.

---
+++ src/test/java/de/adesso/trmclient/cli/TRMCLI/TrmCliApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adesso.trmclient.cli.TRMCLI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrmCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
