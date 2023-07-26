49d3dc0df7e58b61cc8bc1aa8a886c72f12e629a
Ross <jonathan.ross@adesso.de>
2023-07-26 14:38:38 +0200

    Optimize Code.

--- src/main/java/de/adesso/trmclient/cli/TrmCliApplication.java
+++ src/main/java/de/adesso/trmclient/cli/TrmCliApplication.java
@@ -7,24 +7,8 @@ import org.springframework.shell.command.annotation.CommandScan;
 @CommandScan
 @SpringBootApplication
 public class TrmCliApplication {
-	/*
-	 * create sheet "name"
-	 * delete sheet id
-	 * list sheets
-	 *
-	 * show sheet
-	 * filter?
-	 *
-	 * book (erstellt eine Buchung mit Tags (nicht gestartet)
-	 * start -id [tags]
-	 * end -id
-	 * modify
-	 *
-	 *
-	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrmCliApplication.class, args);
 	}
-
 }
